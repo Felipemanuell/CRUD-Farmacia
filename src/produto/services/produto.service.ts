6c1830e7f4c09c7ab1cacd11b03bbee7ed6d1a4d
Felipe Emanuel <fe806497@gmail.com>
2025-10-07 17:37:58 -0300

    Produto finalizado

--- src/produto/services/produto.service.ts
+++ src/produto/services/produto.service.ts
@@ -2,6 +2,7 @@ import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {ILike, Repository} from 'typeorm';
 import {Produto} from '../entitites/produto.entity';
+import { DeleteResult } from 'typeorm/browser';
 
 @Injectable()
 export class ProdutoService {
@@ -32,5 +33,22 @@ export class ProdutoService {
                 nome: ILike(`%${nome}%`)
             }
         });
+    }
+    async create(produto: Produto): Promise<Produto> {
+        return await this.produtoRepository.save(produto);
+    }
+
+    async update(produto: Produto): Promise<Produto> {
+        
+        await this.findById(produto.id);
+
+        return await this.produtoRepository.save(produto);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+
+        await this.findById(id)
+
+        return await this.produtoRepository.delete(id)
     }
 }
